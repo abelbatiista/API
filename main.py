c6f9c8003be6490b9b840cdf2a3bdb6e26f56b9d
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2020-11-30 13:06:13 -0400

    API

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 #CREACION DEL OBJETO APP
 app = FastAPI()
 
 #ORÍGENES DESCONOCIDOS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+
 
 @app.get("/")
 def read_root():

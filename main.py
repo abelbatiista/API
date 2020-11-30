455bf6edef8ae9ca5fe7b3c7d838e3931c9dc0fe
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2020-11-30 13:00:35 -0400

    API

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
-
 @app.get("/")
-def home():
+def read_root():
     return {"message":"Hello TutLinks.com"}
